6f2c4ca5ba2f6b43e6c3d458af85c21109f6bd41
MisterMol <jelled001@gmail.com>
2023-12-07 11:50:31 +0100

    1001 Commit

--- verplaats_fotos.sh
+++ verplaats_fotos.sh
@@ -3,29 +3,75 @@
 echo "Hello"
 
 dir=$1 # Store directory from script parameter in variable
+option=$2 # Store option (week/month) from script parameter in variable
+
+if [ "$option" != "week" ] && [ "$option" != "maand" ]; then
+    echo "Geef een geldige tweede parameter: 'week' of 'maand'"
+    exit 1
+fi
 
 if [ ! -d "$dir" ]; then
     echo "Voer een bestaande directory in"
     exit 1
 fi
 
+destination_dir="/nieuwe_fotos"  # Change this to your desired destination folder
+
+# Check if destination directory exists or create it
+if [ ! -d "$destination_dir" ]; then
+    if mkdir -p "$destination_dir"; then
+        echo "Bestemmingsmap is aangemaakt: $destination_dir"
+    else
+        echo "Kan geen bestemmingsmap maken: $destination_dir"
+        exit 1
+    fi
+fi
+
+# Function to get the week or month number from the file creation date
+get_date_number() {
+    local file_date=$1
+    local format=$2
+    echo $(date -d "$file_date" +"$format")
+}
+
 # Loop through directory
 for file in "$dir"/*
 do
     if [ -f "$file" ]; then
-        # Make directory (if not exists)
-        destination_dir="/nieuwe_fotos"  # Change this to your desired destination folder
-        mkdir -p "$destination_dir"  # -p flag creates parent directories if they don't exist
-        
-        # Copy file to destination
-        cp "$file" "$destination_dir"
-        
         # Get creation date
-        creationdate=$(stat -c "%y" "$file" | awk '{print $1}') # Modification time, change %y to %w for birth time
+        creation_date=$(stat -c "%y" "$file" | awk '{print $1}') # Modification time, change %y to %w for birth time
+        
+        # Get week or month number
+        if [ "$option" == "week" ]; then
+            date_number=$(get_date_number "$creation_date" "%V") # Week number
+        else
+            date_number=$(get_date_number "$creation_date" "%m") # Month number
+        fi
+        
+        # Create subdirectory based on week or month number
+        sub_dir="$destination_dir/$option$date_number"
+        if [ ! -d "$sub_dir" ]; then
+            if mkdir -p "$sub_dir"; then
+                echo "Submap is aangemaakt: $sub_dir"
+            else
+                echo "Kan geen submap maken: $sub_dir"
+                exit 1
+            fi
+        fi
+        
+        # Copy file to destination subdirectory
+        cp "$file" "$sub_dir"
         
         # Check md5sum
-        originalhash=$(md5sum "$file" | awk '{print $1}')
-
-        echo "File: $file, Creation Date: $creationdate, MD5 Hash: $originalhash"
+        original_hash=$(md5sum "$file" | awk '{print $1}')
+        copied_hash=$(md5sum "$sub_dir/$(basename "$file")" | awk '{print $1}')
+        
+        if [ "$original_hash" == "$copied_hash" ]; then
+            # Remove original file if copy was successful
+            rm "$file"
+            echo "File: $file is succesvol verplaatst naar $sub_dir"
+        else
+            echo "Fout bij kopiëren van $file naar $sub_dir. Hashes komen niet overeen."
+        fi
     fi
 done
