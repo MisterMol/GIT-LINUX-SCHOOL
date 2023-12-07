bf14bfc76645b02f44311a6dfc9e08b5a052596d
MisterMol <jelled001@gmail.com>
2023-12-07 12:27:12 +0100

    De Laatste echt

--- verplaats_fotos.sh
+++ verplaats_fotos.sh
@@ -1,52 +1,47 @@
 #!/bin/bash
-# De shebang-lijn specificeert de interpreter om het script uit te voeren.
 
 echo "Hello"
-# Toont "Hello" op de console.
 
-dir=$1 # Bewaar de map van het scriptparameter in een variabele
-option=$2 # Bewaar de optie (week/maand) van het scriptparameter in een variabele
-# Wijs het eerste en tweede commandoregelargument toe aan variabelen 'dir' en 'option'.
+dir=$1
+option=$2
 
 if [ "$option" != "week" ] && [ "$option" != "maand" ]; then
     echo "Geef een geldige tweede parameter: 'week' of 'maand'"
     exit 1
 fi
-# Controleert of de tweede parameter 'week' of 'maand' is. Zo niet, wordt een foutmelding weergegeven en wordt het script beëindigd met status 1.
 
 if [ ! -d "$dir" ]; then
     echo "Voer een bestaande directory in"
     exit 1
 fi
-# Controleert of de opgegeven directory bestaat. Zo niet, wordt een foutmelding weergegeven en wordt het script beëindigd met status 1.
 
-# Loop door de map
-for file in "$dir"/*
-do
+for file in "$dir"/*; do
     if [ -f "$file" ]; then
-        # Controleer of het een foto bestand is (extensies zoals .jpg, .png, .jpeg, etc.)
         if [[ "$file" == *.jpg || "$file" == *.png || "$file" == *.jpeg ]]; then
-            # Haal alleen de bestandsnaam zonder het pad op
             filename=$(basename -- "$file")
-        
-            # Haal de extensie van het bestand op
             extension="${filename##*.}"
-            
-            # Haal de bestandsnaam zonder de extensie op
             filename="${filename%.*}"
 
             # Haal creatiedatum op
-            creation_date=$(stat -c "%y" "$file" | awk '{print $1}') # Aanmaaktijd, verander %y naar %w voor geboortetijd
-
-            # Haal week- of maandnummer op
-            if [ "$option" == "week" ]; then
-                date_number=$(date -d "$creation_date" +"%V") # Weeknummer
+            if [[ "$OSTYPE" == "darwin"* ]]; then
+                # macOS
+                creation_date=$(stat -f "%Sm" -t "%Y-%m-%d" "$file")
             else
-                date_number=$(date -d "$creation_date" +"%m") # Maandnummer
+                # Linux
+                creation_date=$(stat -c "%y" "$file" | awk '{print $1}')
             fi
 
-            # Maak een nieuwe map op basis van week- of maandnummer
-            new_dir="$dir/$option$date_number"
+            # Haal maand- of weeknummer op
+            if [ "$option" == "week" ]; then
+                date_number=$(date -d "$creation_date" +"%V") # Weeknummer
+                new_dir="$dir/Week$date_number"
+            else
+                date_number=$(date -d "$creation_date" +"%m") # Maandnummer
+                month_name=$(date -d "$creation_date" +"%B") # Maandnaam
+                new_dir="$dir/Maand$month_name$date_number"
+            fi
+
+            # Maak een nieuwe map op basis van maand- of weeknummer
             if [ ! -d "$new_dir" ]; then
                 if mkdir -p "$new_dir"; then
                     echo "Nieuwe map is aangemaakt: $new_dir"
@@ -55,11 +50,10 @@ do
                     exit 1
                 fi
             fi
-            
+
             # Verplaats het foto bestand naar de nieuwe map
             mv "$file" "$new_dir/$filename.$extension"
             echo "Bestand: $file is succesvol verplaatst naar $new_dir"
         fi
     fi
 done
-# Doorloopt elk bestand in de opgegeven directory.
